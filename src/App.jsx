083d2a9dfe6027e184ebbd407fcd394b3287fc2e
YashGovindani <yash.govindani@juspay.in>
2026-01-10 12:32:42 +0530

    contact skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Contact from './Contact'
 
 function FlameSvg() {
   return (
@@ -11,7 +12,7 @@ function FlameSvg() {
 
 function LinkedInSvg() {
   return (
-    <svg className='w-full h-full fill-[#ff7a00ff]' fill="currentColor" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    <svg className='w-full h-full fill-[#f46c38]' fill="currentColor" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
       <path d="M28.778 1.004h-25.56c-0.008-0-0.017-0-0.027-0-1.199 0-2.172 0.964-2.186 2.159v25.672c0.014 1.196 0.987 2.161 2.186 2.161 0.010 0 0.019-0 0.029-0h25.555c0.008 0 0.018 0 0.028 0 1.2 0 2.175-0.963 2.194-2.159l0-0.002v-25.67c-0.019-1.197-0.994-2.161-2.195-2.161-0.010 0-0.019 0-0.029 0h0.001zM9.9 26.562h-4.454v-14.311h4.454zM7.674 10.293c-1.425 0-2.579-1.155-2.579-2.579s1.155-2.579 2.579-2.579c1.424 0 2.579 1.154 2.579 2.578v0c0 0.001 0 0.002 0 0.004 0 1.423-1.154 2.577-2.577 2.577-0.001 0-0.002 0-0.003 0h0zM26.556 26.562h-4.441v-6.959c0-1.66-0.034-3.795-2.314-3.795-2.316 0-2.669 1.806-2.669 3.673v7.082h-4.441v-14.311h4.266v1.951h0.058c0.828-1.395 2.326-2.315 4.039-2.315 0.061 0 0.121 0.001 0.181 0.003l-0.009-0c4.5 0 5.332 2.962 5.332 6.817v7.855z"></path>
     </svg>
   )
@@ -19,12 +20,12 @@ function LinkedInSvg() {
 
 function GithubSvg() {
   return (
-    <svg className='w-full h-full fill-[#ff7a00ff]' viewBox="0 0 20 20" fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <svg className='w-full h-full fill-[#f46c38]' viewBox="0 0 20 20" fill="currentColor" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
       <desc>Created with Sketch.</desc>
       <defs>
       </defs>
       <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-          <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)" fill="#ff7a00ff">
+          <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)" fill="#f46c38">
               <g id="icons" transform="translate(56.000000, 160.000000)">
                   <path d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399" id="github-[#142]">
                   </path>
@@ -37,7 +38,7 @@ function GithubSvg() {
 
 function PhoneSvg() {
   return (
-    <svg className='w-full h-full fill-[#ff7a00ff]' version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  fill="currentColor"
+    <svg className='w-full h-full fill-[#f46c38]' version="1.1" id="_x32_" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  fill="currentColor"
       viewBox="0 0 512 512"  xml:space="preserve">
     <g>
       <path d="M500.177,55.798c0,0-21.735-7.434-39.551-11.967C411.686,31.369,308.824,24.727,256,24.727
@@ -60,28 +61,28 @@ function PhoneSvg() {
 
 function MailSvg() {
   return (
-    <svg className='w-full h-full fill-[#ff7a00ff]' viewBox="0 0 192 192" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path stroke="#ff7a00ff" stroke-linejoin="round" stroke-width="12" d="M22 57.265V142c0 5.523 4.477 10 10 10h24V95.056l40 30.278 40-30.278V152h24c5.523 0 10-4.477 10-10V57.265c0-13.233-15.15-20.746-25.684-12.736L96 81.265 47.684 44.53C37.15 36.519 22 44.032 22 57.265Z"/>
+    <svg className='w-full h-full fill-[#f46c38]' viewBox="0 0 192 192" xmlns="http://www.w3.org/2000/svg" fill="currentColor"><path stroke="#f46c38" stroke-linejoin="round" stroke-width="12" d="M22 57.265V142c0 5.523 4.477 10 10 10h24V95.056l40 30.278 40-30.278V152h24c5.523 0 10-4.477 10-10V57.265c0-13.233-15.15-20.746-25.684-12.736L96 81.265 47.684 44.53C37.15 36.519 22 44.032 22 57.265Z"/>
     </svg>
   )
 }
 
 function IdCard() {
   return (
-    <div className='w-full lg:w-[344px] h-fit md:h-[540px] lg:h-[640px] bg-white relative rounded-2xl overflow-hidden'>
+    <div className='w-full lg:w-[344px] h-fit md:h-[540px] lg:h-[640px] lg:flex-none bg-white relative rounded-2xl overflow-hidden'>
       <div className='w-full h-full flex items-center justify-center'>
         <div className='w-full max-w-[344px] h-full bg-transparent relative overflow-visible'>
           <div className='w-full h-full bg-transparent'>
             <div className='w-full h-full bg-transparent flex flex-col py-[30px] px-[20px] gap-[24px] items-center justify-center'>
               <div className='flex-1'>
-                <div className='w-full max-w-[260px] md:w-[240px] h-[210px] md:h-[284px] rounded-2xl bg-orange-500 overflow-hidden'>
+                <div className='w-full max-w-[260px] md:w-[240px] h-[210px] md:h-[284px] rounded-2xl bg-[#f46c38] overflow-hidden'>
                   <img
                     src="https://github.com/YashGovindani/protfolio/blob/1a5d313f9d77d1cf20f986946ac2d2d9f07c9195/IMG_2012.jpeg?raw=true"
                     className="w-full"
                   />
                 </div>
               </div>
-              <div className='flex-1 flex flex-col justify-between items-center'>
-                <div className='text-black text-[36px] font-bold'>Yash Govindani</div>
+              <div className='flex-1 flex flex-col justify-between items-center gap-[16px]'>
+                <div className='text-black text-[36px] leading-[36px] font-bold text-center'>Yash Govindani</div>
                 <div className='flex flex-col gap-[20px]'>
                   <div className='text-[18px] font-medium text-[rgb(106,107,110)] text-center leading-[18px] max-w-[300px]'>
                     A Software Engineer who has developed countless innovative solutions.
@@ -105,14 +106,14 @@ function IdCard() {
             </div>
           </div>
           <div className='absolute -top-4'>
-            <div class="w-[160px] h-[80px] border-4 border-t-transparent border-[rgb(255,122,0)] border-dashed rounded-b-full"></div>
+            <div class="w-[160px] h-[80px] border-4 border-t-transparent border-[rgb(244,108,56)] border-dashed rounded-b-full"></div>
           </div>
         </div>
       </div>
       <div className='absolute bottom-[220px] md:bottom-[170px] -translate-x-2/3 w-fit h-fit rotate-30'>
         <div className='w-full h-full relative'>
-          <div class="w-[232px] h-[101px] border-4 border-t-transparent border-[rgb(255,122,0)] border-dashed rounded-b-full"></div>
-          <div className='w-[34px] h-[34px] absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 -rotate-30 flex items-center justify-center rounded-full bg-[rgb(255,122,0)]'>
+          <div class="w-[232px] h-[101px] border-4 border-t-transparent border-[rgb(244,108,56)] border-dashed rounded-b-full"></div>
+          <div className='w-[34px] h-[34px] absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 -rotate-30 flex items-center justify-center rounded-full bg-[rgb(244,108,56)]'>
             <FlameSvg />
           </div>
         </div>
@@ -123,10 +124,13 @@ function IdCard() {
 
 function App() {
   return (
-    <div className='w-full min-w-[100vw] h-full min-h-[100vh] bg-[rgb(21,19,18)] flex items-center justify-center poppins'>
+    <div className='w-full min-w-[100vw] h-full min-h-[100vh] bg-[rgb(21,19,18)] flex items-center justify-center poppins no-scrollbar'>
       {/* center container */}
-      <div className='w-full lg:w-fit h-fit max-w-[1140px] px-[30px] lg:px-[40px] bg-transparent flex gap-[50px] bg-red-300'>
+      <div className='w-full h-fit max-w-[1140px] p-[30px] lg:p-[40px] bg-transparent flex flex-col lg:flex-row gap-[50px] bg-red-300'>
         <IdCard />
+        <div className='w-full h-fit flex flex-col gap-[120px]'>
+          <Contact />
+        </div>
       </div>
     </div>
   )
---
+++ src/Contact.jsx
@@ -0,0 +1,59 @@
+function Contact() {
+    return (
+        <div className="w-full h-fit flex flex-col bg-transparent gap-[50px]">
+            <div className="w-full h-fit flex flex-col text-[60px] md:text-[90px] font-bold leading-[60px] md:leading-[90px] gap-0 text-center lg:text-left">
+                <div className="text-white">LET'S WORK</div>
+                <div className="text-[rgba(182,180,189,0.2)]">TOGETHER</div>
+            </div>
+            <div className="flex flex-col w-full h-fit gap-[20px] text-[rgb(136,136,136)]">
+                <div className="flex w-full gap-[20px]">
+                    <div className="w-full flex-1 flex flex-col gap-[10px] text-[12px]">
+                        <div className="">Name</div>
+                        <input
+                            id="name"
+                            type="text"
+                            placeholder="Your Name"
+                            className="w-full h-[40px] text-[14px] rounded-[8px] bg-[rgba(182,180,189,0.2)] p-[12px] text-white outline-none focus:ring focus:ring-[rgb(244,108,56)]"
+                        />
+                    </div>
+                    <div className="w-full flex-1 flex flex-col gap-[10px] text-[12px]">
+                        <div className="">Email</div>
+                        <input
+                            id="email"
+                            type="email"
+                            placeholder="your@email.com"
+                            className="w-full h-[40px] text-[14px] rounded-[8px] bg-[rgba(182,180,189,0.2)] p-[12px] text-white outline-none focus:ring focus:ring-[rgb(244,108,56)]"
+                        />
+                    </div>
+                </div>
+                <div className="w-full">
+                    <div className="w-full flex-1 flex flex-col gap-[10px] text-[12px]">
+                        <div className="">Subject</div>
+                        <input
+                            id="subject"
+                            type="text"
+                            placeholder="Can We Connect"
+                            className="w-full h-[40px] text-[14px] rounded-[8px] bg-[rgba(182,180,189,0.2)] p-[12px] text-white outline-none focus:ring focus:ring-[rgb(244,108,56)]"
+                        />
+                    </div>
+                </div>
+                <div className="w-full">
+                    <div className="w-full flex-1 flex flex-col gap-[10px] text-[12px]">
+                        <div className="">Message</div>
+                        <textarea
+                            id="message"
+                            type="text"
+                            placeholder="Message"
+                            className="w-full min-h-[145px] text-[14px] rounded-[8px] bg-[rgba(182,180,189,0.2)] p-[12px] text-white outline-none focus:ring focus:ring-[rgb(244,108,56)] resize-none no-scrollbar"
+                        />
+                    </div>
+                </div>
+                <div className="w-full h-[40px] bg-[rgb(244,108,56)] rounded-[8px] text-white flex items-center justify-center text-[14px] font-semibold">
+                    Submit
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
