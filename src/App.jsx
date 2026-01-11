6cc1159dc957f9cb8f2be2e99128e6e6208553d6
YashGovindani <yash.govindani@juspay.in>
2026-01-12 01:13:07 +0530

    gap change

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,9 @@ function App() {
       <Toaster position="top-middle" />
       <div className='w-full min-w-[100vw] h-full min-h-[100vh] bg-[rgb(21,19,18)] flex items-center justify-center poppins no-scrollbar'>
         {/* center container */}
-        <div className='w-full h-fit max-w-[1140px] p-[30px] lg:p-[40px] bg-transparent flex flex-col lg:flex-row gap-[50px] bg-red-300'>
+        <div className='w-full h-fit max-w-[1140px] p-[30px] lg:p-[40px] bg-transparent flex flex-col lg:flex-row gap-[100px] lg:gap-[50px] bg-red-300'>
           <IdCard />
-          <div className='w-full h-fit flex flex-col gap-[120px]'>
+          <div className='w-full h-fit flex flex-col gap-[100px] lg:gap-[120px]'>
             <Toolbox />
             <Contact />
           </div>
